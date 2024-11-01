28244f6d98020983a975a75727715e030b44faca
Rahul Kumar Singh <ks.rahul0111@gmail.com>
2024-11-02 02:44:11 +0530

    Overview of architecture of NodeJS

---
+++ working-of-nodejs/file.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const os = require("os");
+
+console.log(os.cpus().length); //Find number of cores
+
+/****** Blocking... ******/
+console.log("1");
+console.log("2");
+
+const result = fs.readFileSync("./contacts.txt", "utf-8");
+console.log(result);
+
+console.log("3");
+console.log("4");
+
+// // Output:
+// 1
+// 2
+// Rahul Kumar Singh: +911212121212
+// 3
+// 4
+
+/****** Non-Blocking... ******/
+console.log("1");
+console.log("2");
+
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+  console.log(result);
+});
+
+console.log("3");
+console.log("4");
+
+// // Output:
+// 1
+// 2
+// 3
+// 4
+// Rahul Kumar Singh: +911212121212
+
+// Note: Should always write a code which is non-blocking
